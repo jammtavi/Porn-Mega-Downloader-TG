100de5ece1d48b8272fcd31bbcacc967a9d25cb9
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-12-01 10:48:08 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -117,9 +117,8 @@ def download_progress_hook(d, message, client):
         percent = d.get("_percent_str", "N/A")
         speed = d.get("_speed_str", "N/A")
         to_edit = f"<b><u>Downloading File</b></u> \n<b>File Name :</b> <code>{file_name}</code> \n<b>File Size :</b> <code>{total}</code> \n<b>Speed :</b> <code>{speed}</code> \n<b>ETA :</b> <code>{eta}</code> \n<i>Downloaded {current} out of {total}</i> (__{percent}__)"
-
-        loop = asyncio.get_event_loop()
-        asyncio.run_coroutine_threadsafe(edit_msg(client, message, to_edit), loop)
+        threading.Thread(target=edit_msg, args=(
+            client, message, to_edit)).start()
 
 
 
